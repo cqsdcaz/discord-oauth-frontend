c5f09d982c629051e1ad41fa27c898e4f558ea11
cqsdcaz <cqsdcaz@gmail.com>
2025-09-27 04:13:33 +0200

    test.js

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,10 +1,9 @@
-exports.handler = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ 
-            message: 'Function is working!',
-            path: event.path,
-            method: event.httpMethod
-        })
-    };
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ 
+      message: "Function is working!",
+      timestamp: new Date().toISOString()
+    })
+  };
 };
