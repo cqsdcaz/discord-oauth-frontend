4a00f9606519b481efbdb65009479c88659d0469
cqsdcaz <cqsdcaz@gmail.com>
2025-10-04 18:53:01 +0200

    Delete netlify!/functions directory

--- netlify!/functions/auth.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-    // Simple function to test your setup
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Netlify function is working!' })
-    };
-};
